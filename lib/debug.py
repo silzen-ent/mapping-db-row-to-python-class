56a2056b3ca33840c4a5ad0128bccd50cd907df2
Alan Silva <silzen.ent@gmail.com>
2024-02-14 22:52:51 -0700

    Completed Codealong: Mapping DB Rows to Python Class

--- lib/debug.py
+++ lib/debug.py
@@ -3,12 +3,12 @@ from department import Department
 
 import ipdb
 
-
+# File is set up so that you can explore the DB using the Dept. class from an ipdb sesh.
 def reset_database():
     Department.drop_table()
     Department.create_table()
 
-    Department.create("Payroll", "Building A, 5th Floor")
+    Department.create("Payroll", "Building A, 5th Floor") # Create & saves a new Dept. instance to DB
     Department.create("Human Resources", "Building C, East Wing")
     Department.create("Accounting", "Building B, 1st Floor")
 
